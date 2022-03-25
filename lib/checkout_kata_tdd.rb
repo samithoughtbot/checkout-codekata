3cee9bd91af0e495680aaf150c4dace0a1c437d8
sami <sami@thoughtbot.com>
2022-03-25 09:11:52 +0000

    back to green with no OO design

--- lib/checkout_kata_tdd.rb
+++ lib/checkout_kata_tdd.rb
@@ -6,45 +6,23 @@ class Checkout
     @item_list = []
   end
 
-  def scan(item_id)
-    item = Item.new(item_id, @pricing_rules)
-    @item_list.push(item_id)
+  def scan(item)
+    @item_list.push(item)
 
-    if @pricing_rules[item_id][:special_pricing]
-      has_a_reduction = @item_list.count(item_id) % @pricing_rules[item_id][:special_pricing][:quantity] === 0
+    if has_special_pricing?(item) && @item_list.count(item) % @pricing_rules[item][:special_pricing][:quantity] === 0
+      @total += @pricing_rules[item].fetch(:price) - @pricing_rules[item][:special_pricing][:reduction]
+    else
+      @total += @pricing_rules[item].fetch(:price)
     end
-
-    @total += item.give_me_your_price(has_a_reduction)
   end
 
   def total
     @total
   end
-end
 
-class Item
-  attr_reader :pricing_rules
+  private
 
-  def initialize(item_id, pricing_rules)
-    @item_id = item_id
-    @pricing_rules = pricing_rules
-  end
-  
-  def price_and_reduction
-    [pricing_rules[@item_id].fetch(:price) || 0, pricing_rules[@item_id].fetch(:reduction) || 0]
-  end
-
-  def has_special_pricing?
-    pricing_rules[@item_id][:quantity]
-  end
-
-  def give_me_your_price(has_a_reduction)
-    price, reduction = price_and_reduction
-    if has_special_pricing? && has_a_reduction
-      price - reduction
-    else
-      reduction = 0
-      price - reduction
-    end
+  def has_special_pricing?(item)
+    @pricing_rules[item][:special_pricing]
   end
 end
