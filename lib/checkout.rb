fe8b9c4e5b094d8ec0b7ab7eead9abeb2e63e1d0
sami <sami@thoughtbot.com>
2022-04-29 10:02:31 +0100

    WIP special pricing

--- lib/checkout.rb
+++ lib/checkout.rb
@@ -1,17 +1,34 @@
 class Checkout
-  Items = {}.freeze
+  attr_reader :basket, :pricing_rules
 
-  attr_reader :basket
-
-  def initialize()
+  def initialize(pricing_rules:)
     @basket = []
+    @pricing_rules = pricing_rules
   end
 
   def scan(item)
-    basket.push(item)    
+    if pricing_rules.keys.include?(item)
+      basket.push(item)
+    end
   end
 
   def total
-    basket.sum { |item| Items.fetch(item) }
+    subtotal = 0
+    items = basket.tally # {"A" => 2, "B" => 4}
+
+    items.each do |item, quantity|
+      if pricing_rules.fetch(item).fetch(:special, nil)
+        special = pricing_rules.fetch(item).fetch(:special)
+        quantity, price = special.split(" for ").map(&:to_i)
+        # apply the special price to the relevant quantity
+        # apply unit price to the rest
+        # consider multiple quantities of the special eg. 3 for 130, add 6 to the basket
+        subtotal += price
+      else
+        subtotal += pricing_rules.fetch(item).fetch(:unit) * quantity
+      end
+    end
+
+    subtotal
   end
 end
--- spec/checkout_spec.rb
+++ spec/checkout_spec.rb
@@ -2,22 +2,34 @@ require "spec_helper"
 
 RSpec.describe Checkout do
   describe "#scan" do
-    it "adds the item to the basket" do
-      checkout = Checkout.new
-      tomato_sku = "A"
-
-      checkout.scan(tomato_sku)
-
-      expect(checkout.basket).to eq [tomato_sku]
+    context "when an item has a price" do
+      it "adds the item to the basket" do
+        checkout = Checkout.new(pricing_rules: {"A" => 100})
+        tomato_sku = "A"
+        
+        checkout.scan(tomato_sku)
+        
+        expect(checkout.basket).to eq [tomato_sku]
+      end
+    end
+    
+    context "when an item does not have a price" do
+      it "does not add the item to the basket" do
+        checkout = Checkout.new(pricing_rules: {})
+        tomato_sku = "A"
+        
+        checkout.scan(tomato_sku)
+        
+        expect(checkout.basket).to eq []
+      end
     end
   end
 
   describe "#total" do
-    context "when there is more than 1 item" do
+    context "when there are no special prices" do
       it "returns the total price for all the scanned items" do
-        checkout = Checkout.new
-        items = {"A" => 100, "B" => 250}
-        stub_const("Checkout::Items", items)
+        pricing_rules = {"A" => {unit: 100}, "B" => {unit: 250}}
+        checkout = Checkout.new(pricing_rules: pricing_rules)
         tomato_sku = "A"
         avocado_sku = "B"
 
@@ -26,6 +38,22 @@ RSpec.describe Checkout do
         total = checkout.total
 
         expect(total).to eq 350
+      end
+    end
+
+    context "when there are special prices" do
+      it "returns the total price for all scanned items" do
+        pricing_rules = {"A" => {unit: 100, special: "3 for 130"}}
+        checkout = Checkout.new(pricing_rules: pricing_rules)
+        tomato_sku = "A"
+
+        checkout.scan(tomato_sku)
+        checkout.scan(tomato_sku)
+        checkout.scan(tomato_sku)
+        checkout.scan(tomato_sku)
+        total = checkout.total
+
+        expect(total).to eq 230
       end
     end
   end
